40845722351008ccbe5700d4eabdb69c9037e676
Amol Vyas <54383954+loma373@users.noreply.github.com>
2021-04-05 14:41:59 +0530

    Add files via upload

---
+++ travellingsalesmanproblem.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 08:50:51 2021
+
+@author: Amol
+"""
+
+from sys import maxsize
+from itertools import permutations
+V = 4
+ 
+def tsp(graph, s):
+    vertex = []
+    for i in range(V):
+        if i != s:
+            vertex.append(i)
+    min_path = maxsize
+    next_permutation=permutations(vertex)
+    for i in next_permutation:
+        current_pathweight = 0
+        k = s
+        for j in i:
+            current_pathweight += graph[k][j]
+            k = j
+        current_pathweight += graph[k][s]
+        min_path = min(min_path, current_pathweight)
+    return min_path
+ 
+
+graph = [[0, 10, 15, 20], [10, 0, 35, 25],
+        [15, 35, 0, 30], [20, 25, 30, 0]]
+s = 0
+print(tsp(graph, s))
